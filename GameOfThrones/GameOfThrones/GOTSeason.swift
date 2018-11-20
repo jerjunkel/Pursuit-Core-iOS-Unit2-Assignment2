fbe97d0269f7834f2d061c73be0185dc76e265d6
Jermaine Kelly <jermainekelly@Jermaines-MacBook-Pro.local>
2018-11-20 16:53:14 -0500

    add season struct

---
+++ GameOfThrones/GameOfThrones/GOTSeason.swift
@@ -0,0 +1,13 @@
+//
+//  GOTSeason.swift
+//  GameOfThrones
+//
+//  Created by Jermaine Kelly on 11/20/18.
+//  Copyright © 2018 Pursuit. All rights reserved.
+//
+
+import Foundation
+
+struct Season {
+    
+}
