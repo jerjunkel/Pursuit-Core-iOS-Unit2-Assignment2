85587084eb50e81c4e69e3aef4f93a601b2d0911
Jermaine Kelly <jermainekelly@Jermaines-MacBook-Pro.local>
2018-11-20 23:10:53 -0500

    add injection intializer to episodeVC

--- GameOfThrones/GameOfThrones/EpisodeViewController.swift
+++ GameOfThrones/GameOfThrones/EpisodeViewController.swift
@@ -9,10 +9,19 @@
 import UIKit
 
 class EpisodeViewController: UIViewController {
-
+    private let episode: GOTEpisode
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
     }
     
+    init(episode: GOTEpisode) {
+        self.episode = episode
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
 }
