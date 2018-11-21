b9cc271d6b86b2aabb1620da9c01134feefda789
Jermaine Kelly <jermainekelly@Jermaines-MacBook-Pro.local>
2018-11-20 22:49:13 -0500

    add episodeVC

---
+++ GameOfThrones/GameOfThrones/EpisodeViewController.swift
@@ -0,0 +1,18 @@
+//
+//  EpisodeViewController.swift
+//  GameOfThrones
+//
+//  Created by Jermaine Kelly on 11/20/18.
+//  Copyright © 2018 Pursuit. All rights reserved.
+//
+
+import UIKit
+
+class EpisodeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+}
