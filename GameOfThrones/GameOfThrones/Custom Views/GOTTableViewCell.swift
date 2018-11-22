a9841f2e96498304bd1b2d9c75fbac332d13fa8b
Jermaine Kelly <jermainekelly@Jermaines-MacBook-Pro.local>
2018-11-21 21:13:33 -0500

    add GOTTableviewCell

---
+++ GameOfThrones/GameOfThrones/Custom Views/GOTTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  GOTTableViewCell.swift
+//  GameOfThrones
+//
+//  Created by Jermaine Kelly on 11/21/18.
+//  Copyright © 2018 Pursuit. All rights reserved.
+//
+
+import UIKit
+
+class GOTTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
