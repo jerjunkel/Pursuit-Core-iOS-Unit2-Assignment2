ed5b623fe20d8498d8cc3c3d76395acb4ffc3217
Jermaine Kelly <jermainekelly@Jermaines-MacBook-Pro.local>
2018-11-22 01:23:33 -0500

    change property access level to private

--- GameOfThrones/GameOfThrones/View Controllers/GOTTableViewController.swift
+++ GameOfThrones/GameOfThrones/View Controllers/GOTTableViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 
 class GOTTableViewController: UITableViewController {
-    let seasons = Season.allSeasons
+    private let seasons = Season.allSeasons
     
     override func viewDidLoad() {
         super.viewDidLoad()
